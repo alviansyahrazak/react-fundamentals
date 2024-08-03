092f99fa0456aaab9905eca0d2b906b0097b23f4
alviansyahrazak <alviansyahrazak@gmail.com>
2024-08-03 16:09:48 +0800

    feat: fetch API

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import postsData from '../posts.json';
 import { Article } from '../components/Article';
 import { Search } from '../components/Search';
@@ -7,6 +7,7 @@ import { Search } from '../components/Search';
 export const Homepage = () => {
   const [posts, setPosts] = useState(postsData);
   const [totalPosts, setTotalPosts] = useState(0);
+  const [externalPosts, setExternalPosts] = useState([]);
 
   const onSearchClick = (value) => {
     const filteredPosts = postsData.filter((item) =>
@@ -16,12 +17,23 @@ export const Homepage = () => {
     setTotalPosts(filteredPosts.length);
   };
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => setExternalPosts(data));
+  }, []);
+
   return (
     <>
       <h1>Simple Blog</h1>
       <Search onSearchClick={onSearchClick} totalPosts={totalPosts} />
       {posts.map(({ title, tags, date, isNew }, index) => (
         <Article {...{ title, tags, date, isNew }} key={index} />
+      ))}
+      <hr />
+      <h1>External Posts</h1>
+      {externalPosts.map((item, id) => (
+        <div key={id}> {item.title}</div>
       ))}
     </>
   );
